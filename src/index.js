fc519bd4eb14d1e112895aef9fa3f3b34fabd1dc
Kimyeokyoung <kyk9412@hanmail.net>
2024-12-02 21:04:19 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Schedule from "./components/schedules/Schedule";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<Routes>
+				<Route index element={<App />} />
+				<Route path="/schedule" element={<Schedule />} />
+			</Routes>
+		</BrowserRouter>
 	</React.StrictMode>
 );
