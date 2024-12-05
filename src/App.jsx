1c4b83716a1a246b4672cb2a32e5732cfe7fce9b
Kimyeokyoung <kyk9412@hanmail.net>
2024-12-05 23:49:34 +0900

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import './style.css'
-import Nav from './components/Nav'
-import Header from './components/Header'
-import Main from './components/Main'
+import "./style.css";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import { useState } from "react";
 
 function App() {
-  return (
-    <body>
-      <Header />
-      <Nav />
-      <Main />
-    </body>
-  )
+	const [modalState, setModalState] = useState(false);
+	return (
+		<>
+			<Header setModalState={setModalState} />
+			<Nav modalState={modalState} setModalState={setModalState} />
+			<Main />
+		</>
+	);
 }
 
-export default App
+export default App;
