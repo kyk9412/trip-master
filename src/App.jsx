087ab2370822f1d8f18b7ec120426cacff20869e
Kimyeokyoung <kyk9412@hanmail.net>
2024-12-06 23:34:56 +0900

    Split components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import './style.css'
-import Nav from './components/Nav'
-import Header from './components/Header'
-import Main from './components/Main'
-import { useState } from 'react'
+import "./style.css";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import { useState } from "react";
 
 function App() {
-  const [modalState, setModalState] = useState(false)
-  return (
-    <>
-      <Header setModalState={setModalState} />
-      <Nav modalState={modalState} setModalState={setModalState} />
-      <Main />
-    </>
-  )
+	const [modalState, setModalState] = useState(false);
+	return (
+		<>
+			<Header setModalState={setModalState} />
+			<Nav modalState={modalState} setModalState={setModalState} />
+			<Main />
+		</>
+	);
 }
 
-export default App
+export default App;
