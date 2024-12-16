e515ddebcd6c91f6ac8533b1ecae7402378a3271
Kimyeokyoung <kyk9412@hanmail.net>
2024-12-16 22:07:31 +0900

    citypage에 있는 상태관리 stepstatus로 통합관리

--- src/components/schedules/CityPage/CityContents.jsx
+++ src/components/schedules/CityPage/CityContents.jsx
@@ -1,21 +1,28 @@
-import CityContentOne from './CityContentOne'
-import CityContentTwo from './CityContentTwo'
-import CityContentThree from './CityContentThree'
-import CityContentFour from './CityContentFour'
-import CityContentFive from './CityContentFive'
-import CityContentSix from './CityContentSix'
+import CityContentOne from "./CityContentOne";
+import CityContentTwo from "./CityContentTwo";
+import CityContentThree from "./CityContentThree";
+import CityContentFour from "./CityContentFour";
+import CityContentFive from "./CityContentFive";
+import CityContentSix from "./CityContentSix";
 
-function CityContents({ progressNumber }) {
-  return (
-    <div class="main-wrap">
-      {progressNumber === 1 && <CityContentOne />}
-      {progressNumber === 2 && <CityContentTwo />}
-      {progressNumber === 3 && <CityContentThree />}
-      {progressNumber === 4 && <CityContentFour />}
-      {progressNumber === 5 && <CityContentFive />}
-      {progressNumber === 6 && <CityContentSix />}
-    </div>
-  )
+function CityContents({ stepStatus, setStepStatus }) {
+	return (
+		<div className="main-wrap">
+			{stepStatus.progress === 1 && (
+				<CityContentOne setStepStatus={setStepStatus} />
+			)}
+			{stepStatus.progress === 2 && (
+				<CityContentTwo
+					stepStatus={stepStatus}
+					setStepStatus={setStepStatus}
+				/>
+			)}
+			{stepStatus.progress === 3 && <CityContentThree />}
+			{stepStatus.progress === 4 && <CityContentFour />}
+			{stepStatus.progress === 5 && <CityContentFive />}
+			{stepStatus.progress === 6 && <CityContentSix />}
+		</div>
+	);
 }
 
-export default CityContents
+export default CityContents;
--- src/components/schedules/CityPage/CityProgress.jsx
+++ src/components/schedules/CityPage/CityProgress.jsx
@@ -1,9 +1,9 @@
-function CityProgress({ progressNumber }) {
-  return (
-    <div class="nav_bar">
-      <div class="nav_number">{progressNumber}/6</div>
-    </div>
-  )
+function CityProgress({ stepStatus }) {
+	return (
+		<div class="nav_bar">
+			<div class="nav_number">{stepStatus.progress}/6</div>
+		</div>
+	);
 }
 
-export default CityProgress
+export default CityProgress;
