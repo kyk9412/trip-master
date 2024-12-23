5696d5c816dec9bb73717aa01c9173114482ad6b
Kimyeokyoung <kyk9412@hanmail.net>
2024-12-23 15:59:38 +0900

    feat(react): generate components

--- src/components/schedules/CityPage/CityContentTwoDomestic.jsx
+++ src/components/schedules/CityPage/CityContentTwoDomestic.jsx
@@ -1,7 +1,23 @@
+import AreaButton from "../../../common/AreaButton";
+
 function CityContentTwoDomestic() {
+	const cityList = [
+		{ text: "가평'양평", id: "gapyeong" },
+		{ text: "강릉'속초", id: "gangneung" },
+		{ text: "경주", id: "gyeongju" },
+		{ text: "부산", id: "busan" },
+		{ text: "여수", id: "yeosu" },
+		{ text: "인천", id: "incheon" },
+		{ text: "제주", id: "jeonju" },
+		{ text: "춘천'홍천", id: "chuncheon" },
+		{ text: "태안", id: "taean" },
+		{ text: "통영'거제'남해", id: "tongyeong" },
+		{ text: "포항'안동", id: "pohang" },
+	];
+
 	return (
 		<>
-			<div className="container_textbox">
+			<div className="container_texgapyeongtbox">
 				<img src="images/airplane.png" alt="" />
 				<h2 className="title">국내에서 떠나고 싶은 곳은?</h2>
 				<span className="text">1곳을 선택해주세요.</span>
@@ -9,90 +25,15 @@ function CityContentTwoDomestic() {
 			<div className="korea">
 				<p>대한민국</p>
 				<div className="korea_area">
-					<div className="area">
-						<input type="checkbox" name="gapyeong" id="gapyeong" />
-						<label htmlFor="gapyeong" className="citytext">
-							가평'양평
-						</label>
-					</div>
-					<div className="area">
-						<input
-							type="checkbox"
-							name="gangneung"
-							id="gangneung"
-						/>
-						<label htmlFor="gangneung" className="citytext">
-							강릉'속초
-						</label>
-					</div>
-					<div className="area">
-						<input type="checkbox" name="gyeongju" id="gyeongju" />
-						<label htmlFor="gyeongju" className="citytext">
-							경주
-						</label>
-					</div>
-					<div className="area">
-						<input type="checkbox" name="busan" id="busan" />
-						<label htmlFor="busan" className="citytext">
-							부산
-						</label>
-					</div>
-					<div className="area">
-						<input type="checkbox" name="yeosu" id="yeosu" />
-						<label htmlFor="yeosu" className="citytext">
-							여수
-						</label>
-					</div>
-					<div className="area">
-						<input type="checkbox" name="incheon" id="incheon" />
-						<label htmlFor="incheon" className="citytext">
-							인천
-						</label>
-					</div>
-					<div className="area">
-						<input type="checkbox" name="jeonju" id="jeonju" />
-						<label htmlFor="jeonju" className="citytext">
-							전주
-						</label>
-					</div>
-					<div className="area">
-						<input type="checkbox" name="jeju" id="jeju" />
-						<label htmlFor="jeju" className="citytext">
-							제주
-						</label>
-					</div>
-					<div className="area">
-						<input
-							type="checkbox"
-							name="chuncheon"
-							id="chuncheon"
-						/>
-						<label htmlFor="chuncheon" className="citytext">
-							춘천'홍천
-						</label>
-					</div>
-					<div className="area">
-						<input type="checkbox" name="taean" id="taean" />
-						<label htmlFor="taean" className="citytext">
-							태안
-						</label>
-					</div>
-					<div className="area">
-						<input
-							type="checkbox"
-							name="tongyeong"
-							id="tongyeong"
-						/>
-						<label htmlFor="tongyeong" className="citytext">
-							통영'거제'남해
-						</label>
-					</div>
-					<div className="area">
-						<input type="checkbox" name="pohang" id="pohang" />
-						<label htmlFor="pohang" className="citytext">
-							포항'안동
-						</label>
-					</div>
+					{cityList.map((city, index) => {
+						return (
+							<AreaButton
+								key={city.id}
+								text={city.text}
+								id={city.id}
+							/>
+						);
+					})}
 				</div>
 			</div>
 		</>
