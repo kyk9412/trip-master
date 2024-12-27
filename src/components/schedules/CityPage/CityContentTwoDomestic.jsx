9878cc36fe1f62386a3665730f4644c791b50ba9
Kimyeokyoung <kyk9412@hanmail.net>
2024-12-27 14:29:29 +0900

    feat: generate components

--- src/components/schedules/CityPage/CityContentTwoDomestic.jsx
+++ src/components/schedules/CityPage/CityContentTwoDomestic.jsx
@@ -1,105 +1,39 @@
-import React from "react";
-import Style from "../../../styles/CityContentTwoDomestic.module.css";
+import AreaButton from "../../../common/AreaButton";
 
 function CityContentTwoDomestic() {
+	const cityList = [
+		{ text: "가평'양평", id: "gapyeong" },
+		{ text: "강릉'속초", id: "gangneung" },
+		{ text: "경주", id: "gyeongju" },
+		{ text: "부산", id: "busan" },
+		{ text: "여수", id: "yeosu" },
+		{ text: "인천", id: "incheon" },
+		{ text: "전주", id: "jeonju" },
+		{ text: "제주", id: "jeju" },
+		{ text: "춘천'홍천", id: "chuncheon" },
+		{ text: "태안", id: "taean" },
+		{ text: "통영'거제'남해", id: "tongyeong" },
+		{ text: "포항'안동", id: "pohang" },
+	];
 	return (
 		<>
-			<div className={Style.container_textbox}>
-				<img
-					src="/images/airplane.png"
-					style={{ width: "50px" }}
-					alt=""
-				/>
-				<h2 className={Style.title}>국내에서 떠나고 싶은 곳은?</h2>
-				<span className={Style.text}>1곳을 선택해주세요.</span>
+			<div className="container_textbox">
+				<img src="images/airplane.png" alt="" />
+				<h2 className="title">국내에서 떠나고 싶은 곳은?</h2>
+				<span className="text">1곳을 선택해주세요.</span>
 			</div>
-			<div className={Style.korea}>
+			<div className="korea">
 				<p>대한민국</p>
-				<div className={Style.korea_area}>
-					<div className={Style.area}>
-						<input type="checkbox" name="gapyeong" id="gapyeong" />
-						<label htmlFor="gapyeong" className={Style.citytext}>
-							가평'양평
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input
-							type="checkbox"
-							name="gangneung"
-							id="gangneung"
-						/>
-						<label htmlFor="gangneung" className={Style.citytext}>
-							강릉'속초
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="gyeongju" id="gyeongju" />
-						<label htmlFor="gyeongju" className={Style.citytext}>
-							경주
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="busan" id="busan" />
-						<label htmlFor="busan" className={Style.citytext}>
-							부산
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="yeosu" id="yeosu" />
-						<label htmlFor="yeosu" className={Style.citytext}>
-							여수
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="incheon" id="incheon" />
-						<label htmlFor="incheon" className={Style.citytext}>
-							인천
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="jeonju" id="jeonju" />
-						<label htmlFor="jeonju" className={Style.citytext}>
-							전주
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="jeju" id="jeju" />
-						<label htmlFor="jeju" className={Style.citytext}>
-							제주
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input
-							type="checkbox"
-							name="chuncheon"
-							id="chuncheon"
-						/>
-						<label htmlFor="chuncheon" className={Style.citytext}>
-							춘천'홍천
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="taean" id="taean" />
-						<label htmlFor="taean" className={Style.citytext}>
-							태안
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input
-							type="checkbox"
-							name="tongyeong"
-							id="tongyeong"
-						/>
-						<label htmlFor="tongyeong" className={Style.citytext}>
-							통영'거제'남해
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="pohang" id="pohang" />
-						<label htmlFor="pohang" className={Style.citytext}>
-							포항'안동
-						</label>
-					</div>
+				<div className="korea_area">
+					{cityList.map((city, index) => {
+						return (
+							<AreaButton
+								key={city.id}
+								text={city.text}
+								id={city.id}
+							/>
+						);
+					})}
 				</div>
 			</div>
 		</>
--- src/components/schedules/CityPage/CityContentTwoForeign.jsx
+++ src/components/schedules/CityPage/CityContentTwoForeign.jsx
@@ -1,463 +1,115 @@
-import React from "react";
-import Style from "../../../styles/CityContentTwoForeign.module.css";
+import AreaButton from "../../../common/AreaButton";
+// import Style from "../../../styles/CityContentTwoForeign.module.css";
 
 function CityContentTwoForeign() {
+	const cityList = [
+		{ text: "도쿄", id: "tokyo" },
+		{ text: "후쿠오카", id: "fukuoka" },
+		{ text: "오사카", id: "osaka" },
+		{ text: "시즈오카", id: "shizuoka" },
+		{ text: "나고야", id: "nagoya" },
+		{ text: "삿포로", id: "sapporo" },
+		{ text: "오키나와", id: "okinawa" },
+		{ text: "가오슝", id: "kaohsiung" },
+		{ text: "칭다오", id: "qingdao" },
+		{ text: "홍콩", id: "hongkong" },
+		{ text: "타이베이", id: "taipei" },
+		{ text: "상하이", id: "shanghai" },
+		{ text: "베이징", id: "beijing" },
+		{ text: "그라나다", id: "granada" },
+		{ text: "두브로브니크", id: "dubrovnik" },
+		{ text: "리스본", id: "lisbon" },
+		{ text: "밀라논", id: "milan" },
+		{ text: "브뤼셀", id: "brussels" },
+		{ text: "세비야", id: "seville" },
+		{ text: "포르투", id: "porto" },
+		{ text: "파리", id: "paris" },
+		{ text: "프라하", id: "prague" },
+		{ text: "로마", id: "rome" },
+		{ text: "런던", id: "london" },
+		{ text: "바르셀로나", id: "barcelona" },
+		{ text: "빈", id: "vienna" },
+		{ text: "피렌체", id: "florence" },
+		{ text: "인터라켄", id: "interlaken" },
+		{ text: "마드리드", id: "madrid" },
+		{ text: "부다페스트", id: "budapest" },
+		{ text: "블라디보스토크", id: "vladivostok" },
+		{ text: "취리히", id: "zurich" },
+		{ text: "프랑크푸르트", id: "frankfurt" },
+		{ text: "뮌헨", id: "munich" },
+		{ text: "암스테르담", id: "amsterdam" },
+		{ text: "베를린", id: "berlin" },
+		{ text: "나트랑", id: "nhatrang" },
+		{ text: "치앙마이", id: "chiangmai" },
+		{ text: "푸꾸옥", id: "phuquoc" },
+		{ text: "라오스", id: "laos" },
+		{ text: "쿠알라룸푸르", id: "kualalumpur" },
+		{ text: "달랏", id: "dalat" },
+		{ text: "다낭", id: "danang" },
+		{ text: "방콕", id: "bangkok" },
+		{ text: "세부", id: "cebu" },
+		{ text: "코타키나발루", id: "kotaKinabalu" },
+		{ text: "싱가포르", id: "singapore" },
+		{ text: "하노이", id: "hanoi" },
+		{ text: "호치민", id: "hochiminh" },
+		{ text: "발리", id: "bali" },
+		{ text: "푸켓", id: "phuket" },
+		{ text: "보라카이", id: "boracay" },
+		{ text: "두바이", id: "dubai" },
+		{ text: "벤쿠버", id: "vancouver" },
+		{ text: "샌프란시스코", id: "sanfrancisco" },
+		{ text: "토론토", id: "toronto" },
+		{ text: "하와이", id: "hawaii" },
+		{ text: "뉴욕", id: "newyork" },
+		{ text: "로스앤젤레스", id: "losangeles" },
+		{ text: "시드니", id: "sydney" },
+		{ text: "멜버른", id: "melbourne" },
+		{ text: "괌", id: "guam" },
+		{ text: "사이판", id: "saipan" },
+	];
 	return (
 		<>
-			<div className={Style.container_textbox}>
-				<img
-					src="/images/airplane.png"
-					style={{ width: "50px" }}
-					alt=""
-				/>
-				<h2 className={Style.title}>해외에서 떠나고 싶은 곳은?</h2>
-				<span className={Style.text}>1곳을 선택해주세요.</span>
+			<div className="container_textbox">
+				<img src="images/airplane.png" alt="" />
+				<h2 className="title">해외에서 떠나고 싶은 곳은?</h2>
+				<span className="text">1곳을 선택해주세요.</span>
 			</div>
-			<div className={Style.foreign}>
-				<div className={Style.foreign_area}>
-					<p className={Style.country}>일본</p>
-					<div className={Style.area}>
-						<input type="checkbox" name="tokyo" id="tokyo" />
-						<label htmlFor="tokyo" className={Style.citytext}>
-							도쿄
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="fukuoka" id="fukuoka" />
-						<label htmlFor="fukuoka" className={Style.citytext}>
-							후쿠오카
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="osaka" id="osaka" />
-						<label htmlFor="osaka" className={Style.citytext}>
-							오사카
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="shizuoka" id="shizuoka" />
-						<label htmlFor="shizuoka" className={Style.citytext}>
-							시즈오카
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="nagoya" id="nagoya" />
-						<label htmlFor="nagoya" className={Style.citytext}>
-							나고야
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="sapporo" id="sapporo" />
-						<label htmlFor="sapporo" className={Style.citytext}>
-							삿포로
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="okinawa" id="okinawa" />
-						<label htmlFor="okinawa" className={Style.citytext}>
-							오키나와
-						</label>
-					</div>
+			<div className="foreign">
+				<div className="foreign_area">
+					<p className="country">일본</p>
+					{cityList.map((city, index) => {
+						return (
+							<AreaButton
+								Key={city.id}
+								text={city.text}
+								id={city.id}
+							/>
+						);
+					})}
 				</div>
 
-				<div className={Style.foreign_area}>
-					<p className={Style.country}>중화/중국</p>
-					<div className={Style.area}>
-						<input
-							type="checkbox"
-							name="kaohsiung"
-							id="kaohsiung"
-						/>
-						<label htmlFor="kaohsiung" className={Style.citytext}>
-							가오슝
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="qingdao" id="qingdao" />
-						<label htmlFor="qingdao" className={Style.citytext}>
-							칭다오
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="hongkong" id="hongkong" />
-						<label htmlFor="hongkong" className={Style.citytext}>
-							홍콩
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="taipei" id="taipei" />
-						<label htmlFor="taipei" className={Style.citytext}>
-							타이베이
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="shanghai" id="shanghai" />
-						<label htmlFor="shanghai" className={Style.citytext}>
-							상하이
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="beijing" id="beijing" />
-						<label htmlFor="beijing" className={Style.citytext}>
-							베이징
-						</label>
-					</div>
+				<div className="foreign_area">
+					<p className="country">중화/중국</p>
 				</div>
 
-				<div className={Style.foreign_area}>
-					<p className={Style.country}>유럽</p>
-					<div className={Style.area}>
-						<input type="checkbox" name="granada" id="granada" />
-						<label htmlFor="granada" className={Style.citytext}>
-							그라나다
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input
-							type="checkbox"
-							name="dubrovnik"
-							id="dubrovnik"
-						/>
-						<label htmlFor="dubrovnik" className={Style.citytext}>
-							두브로브니크
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="lisbon" id="lisbon" />
-						<label htmlFor="lisbon" className={Style.citytext}>
-							리스본
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="milan" id="milan" />
-						<label htmlFor="milan" className={Style.citytext}>
-							밀라논
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="brussels" id="brussels" />
-						<label htmlFor="brussels" className={Style.citytext}>
-							브뤼셀
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="seville" id="seville" />
-						<label htmlFor="seville" className={Style.citytext}>
-							세비야
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="porto" id="porto" />
-						<label htmlFor="porto" className={Style.citytext}>
-							포르투
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="paris" id="paris" />
-						<label htmlFor="paris" className={Style.citytext}>
-							파리
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="prague" id="prague" />
-						<label htmlFor="prague" className={Style.citytext}>
-							프라하
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="rome" id="rome" />
-						<label htmlFor="rome" className={Style.citytext}>
-							로마
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="london" id="london" />
-						<label htmlFor="london" className={Style.citytext}>
-							런던
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input
-							type="checkbox"
-							name="barcelona"
-							id="barcelona"
-						/>
-						<label htmlFor="barcelona" className={Style.citytext}>
-							바르셀로나
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="vienna" id="vienna" />
-						<label htmlFor="vienna" className={Style.citytext}>
-							빈
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="florence" id="florence" />
-						<label htmlFor="florence" className={Style.citytext}>
-							피렌체
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input
-							type="checkbox"
-							name="interlaken"
-							id="interlaken"
-						/>
-						<label htmlFor="interlaken" className={Style.citytext}>
-							인터라켄
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="madrid" id="madrid" />
-						<label htmlFor="madrid" className={Style.citytext}>
-							마드리드
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="budapest" id="budapest" />
-						<label htmlFor="budapest" className={Style.citytext}>
-							부다페스트
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input
-							type="checkbox"
-							name="vladivostok"
-							id="vladivostok"
-						/>
-						<label htmlFor="vladivostok" className={Style.citytext}>
-							블라디보스토크
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="zurich" id="zurich" />
-						<label htmlFor="zurich" className={Style.citytext}>
-							취리히
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input
-							type="checkbox"
-							name="frankfurt"
-							id="frankfurt"
-						/>
-						<label htmlFor="frankfurt" className={Style.citytext}>
-							프랑크푸르트
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="munich" id="munich" />
-						<label htmlFor="munich" className={Style.citytext}>
-							뮌헨
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input
-							type="checkbox"
-							name="amsterdam"
-							id="amsterdam"
-						/>
-						<label htmlFor="amsterdam" className={Style.citytext}>
-							암스테르담
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="berlin" id="berlin" />
-						<label htmlFor="berlin" className={Style.citytext}>
-							베를린
-						</label>
-					</div>
+				<div className="foreign_area">
+					<p className="country">유럽</p>
 				</div>
 
-				<div className={Style.foreign_area}>
-					<p className={Style.country}>동남아시아</p>
-					<div className={Style.area}>
-						<input type="checkbox" name="gapyeong" id="gapyeong" />
-						<label htmlFor="gapyeong" className={Style.citytext}>
-							나트랑
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input
-							type="checkbox"
-							name="gangneung"
-							id="gangneung"
-						/>
-						<label htmlFor="gangneung" className={Style.citytext}>
-							치앙마이
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="gyeongju" id="gyeongju" />
-						<label htmlFor="gyeongju" className={Style.citytext}>
-							푸꾸옥
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="busan" id="busan" />
-						<label htmlFor="busan" className={Style.citytext}>
-							라오스
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="yeosu" id="yeosu" />
-						<label htmlFor="yeosu" className={Style.citytext}>
-							쿠알라룸푸르
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="incheon" id="incheon" />
-						<label htmlFor="incheon" className={Style.citytext}>
-							달랏
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="incheon" id="incheon" />
-						<label htmlFor="incheon" className={Style.citytext}>
-							다낭
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="incheon" id="incheon" />
-						<label htmlFor="incheon" className={Style.citytext}>
-							방콕
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="incheon" id="incheon" />
-						<label htmlFor="incheon" className={Style.citytext}>
-							세부
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="incheon" id="incheon" />
-						<label htmlFor="incheon" className={Style.citytext}>
-							코타키나발루
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="incheon" id="incheon" />
-						<label htmlFor="incheon" className={Style.citytext}>
-							싱가포르
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="incheon" id="incheon" />
-						<label htmlFor="incheon" className={Style.citytext}>
-							하노이
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="incheon" id="incheon" />
-						<label htmlFor="incheon" className={Style.citytext}>
-							호치민
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="incheon" id="incheon" />
-						<label htmlFor="incheon" className={Style.citytext}>
-							발리
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="incheon" id="incheon" />
-						<label htmlFor="incheon" className={Style.citytext}>
-							푸켓
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="incheon" id="incheon" />
-						<label htmlFor="incheon" className={Style.citytext}>
-							보라카이
-						</label>
-					</div>
+				<div className="foreign_area">
+					<p className="country">동남아시아</p>
 				</div>
 
-				<div className={Style.foreign_area}>
-					<p className={Style.country}>서아시아</p>
-					<div className={Style.area}>
-						<input type="checkbox" name="gapyeong" id="gapyeong" />
-						<label htmlFor="gapyeong" className={Style.citytext}>
-							두바이
-						</label>
-					</div>
+				<div className="foreign_area">
+					<p className="country">서아시아</p>
 				</div>
 
-				<div className={Style.foreign_area}>
-					<p className={Style.country}>미주</p>
-					<div className={Style.area}>
-						<input type="checkbox" name="gapyeong" id="gapyeong" />
-						<label htmlFor="gapyeong" className={Style.citytext}>
-							벤쿠버
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input
-							type="checkbox"
-							name="gangneung"
-							id="gangneung"
-						/>
-						<label htmlFor="gangneung" className={Style.citytext}>
-							샌프란시스코
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="gyeongju" id="gyeongju" />
-						<label htmlFor="gyeongju" className={Style.citytext}>
-							토론토
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="busan" id="busan" />
-						<label htmlFor="busan" className={Style.citytext}>
-							하와이
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="yeosu" id="yeosu" />
-						<label htmlFor="yeosu" className={Style.citytext}>
-							뉴욕
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="incheon" id="incheon" />
-						<label htmlFor="incheon" className={Style.citytext}>
-							로스앤젤레스
-						</label>
-					</div>
+				<div className="foreign_area">
+					<p className="country">미주</p>
 				</div>
 
-				<div className={Style.foreign_area}>
-					<p className={Style.country}>미주</p>
-					<div className={Style.area}>
-						<input type="checkbox" name="gapyeong" id="gapyeong" />
-						<label htmlFor="gapyeong" className={Style.citytext}>
-							시드니
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input
-							type="checkbox"
-							name="gangneung"
-							id="gangneung"
-						/>
-						<label htmlFor="gangneung" className={Style.citytext}>
-							멜버른
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="gyeongju" id="gyeongju" />
-						<label htmlFor="gyeongju" className={Style.citytext}>
-							괌
-						</label>
-					</div>
-					<div className={Style.area}>
-						<input type="checkbox" name="busan" id="busan" />
-						<label htmlFor="busan" className={Style.citytext}>
-							사이판
-						</label>
-					</div>
+				<div className="foreign_area">
+					<p className="country">미주</p>
 				</div>
 			</div>
 		</>
